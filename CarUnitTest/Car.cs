d01e81da0db9f5e84ecac41b7ae4273b36a74ea3
Asland3 <malthe3@outlook.com>
2022-03-06 22:49:20 +0100

    Add project files.

---
+++ CarUnitTest/Car.cs
@@ -0,0 +1,56 @@
+﻿namespace CarUnitTest
+{
+    public class Car
+    {
+        private int _id;
+        private string _model;
+        private int _price;
+        private string _licensePlate;
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+            ID = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+        public int ID
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "Name is null");
+                if (value.Length <= 4) throw new ArgumentOutOfRangeException("Name must be at least 4 characters");
+                _model = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Price", value, "is out of range");
+                _price = value;
+            }
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                if (value.Length < 2 || value.Length > 7) throw new ArgumentOutOfRangeException(nameof(value));
+                _licensePlate = value;
+            }
+        }
+    }
+}
---
+++ CarUnitTestTests/CarTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CarUnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarUnitTestTests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _car = new Car(100, "Toyota", 500000, "BK75590");
+        }
+
+        [TestMethod()]
+        public void ModelTest()
+        {
+            Assert.AreEqual("Toyota", _car.Model);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Model = null);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Model = "BMW");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            _car.Price = 50;
+            Assert.AreEqual(50, _car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = -1);
+        }
+
+        [TestMethod()]
+        public void LicensePlateTest()
+        {
+            _car.LicensePlate = "BD75590";
+            Assert.AreEqual("BD75590", _car.LicensePlate);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "a");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "abcd265989");
+            Assert.ThrowsException<ArgumentNullException>(() => _car.LicensePlate = null);
+        }
+
+
+    }
+}
